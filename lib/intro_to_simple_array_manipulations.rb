56f60fb52c654ac6007273bde777946ebe30f64b
Justin Wang <justinw827@aol.com>
2018-07-18 01:49:28 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(aIn, sIn)
+  aIn.push(sIn)
+end
+
+def using_unshift(aIn, sIn)
+  aIn.unshift(sIn)
+end
+
+def using_pop(aIn)
+  aIn.pop
+end
+
+def pop_with_args(aIn)
+  aIn.pop(2)
+end
+
+def using_shift(aIn)
+  aIn.shift
+end
+
+def shift_with_args(aIn)
+  aIn.shift(2)
+end
+
+def using_concat(aIn, a2In)
+  aIn.concat(a2In)
+end
+
+def using_insert(aIn, eIn)
+  aIn.insert(4, eIn)
+end
+
+def using_uniq(aIn)
+  aIn.uniq
+end
+
+def using_flatten(aIn)
+  aIn.flatten
+end
+
+def using_delete(aIn, sIn)
+  aIn.delete(sIn)
+end
+
+def using_delete_at(aIn, nIn)
+  aIn.delete_at(nIn)
+end
